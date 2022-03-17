e03c36442df76cc6ef2ae408ee8ff629f8e41216
Peter <necco.huang@goldenf.co>
2022-03-17 15:05:50 +0800

    fix api template

--- txpay/api/internal/config/config.go
+++ txpay/api/internal/config/config.go
@@ -1,7 +1,24 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/rest"
+)
 
 type Config struct {
 	rest.RestConf
+	service.ServiceConf
+	Mysql struct {
+		Host       string
+		Port       int
+		DBName     string
+		UserName   string
+		Password   string
+		DebugLevel string
+	}
+	RedisCache struct {
+		RedisSentinelNode string
+		RedisMasterName   string
+		RedisDB           int
+	}
 }
--- txpay/api/internal/svc/servicecontext.go
+++ txpay/api/internal/svc/servicecontext.go
@@ -1,15 +1,44 @@
 package svc
 
 import (
+	"fmt"
 	"github.com/copo888/channel_app/txpay/api/internal/config"
+	"github.com/go-redis/redis/v8"
+	"github.com/neccoys/go-driver/mysqlx"
+	"gorm.io/gorm"
+	"log"
+	"strings"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config      config.Config
+	RedisClient *redis.Client
+	MyDB        *gorm.DB
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+
+	// Redis
+	redisCache := redis.NewFailoverClient(&redis.FailoverOptions{
+		MasterName:    c.RedisCache.RedisMasterName,
+		SentinelAddrs: strings.Split(c.RedisCache.RedisSentinelNode, ";"),
+		DB:            c.RedisCache.RedisDB,
+	})
+
+	log.Println(c.Mysql)
+	// DB
+	db, err := mysqlx.New(c.Mysql.Host, fmt.Sprintf("%d", c.Mysql.Port), c.Mysql.UserName, c.Mysql.Password, c.Mysql.DBName).
+		SetCharset("utf8mb4").
+		SetLoc("UTC").
+		Connect(mysqlx.Pool(50, 100, 180))
+
+	if err != nil {
+		panic(err)
+	}
+
 	return &ServiceContext{
-		Config: c,
+		Config:      c,
+		RedisClient: redisCache,
+		MyDB:        db,
 	}
 }
