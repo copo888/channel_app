370c64a223b6cfddcdd8d631fb6d64cec5a6dc6f
Kenny <kenny@e-pay.vip>
2022-06-13 08:40:13 +0800

    add wanlisinpay

---
+++ wanlisinpay/internal/logic/payorderlogic.go
@@ -0,0 +1,130 @@
+package logic
+
+import (
+	"context"
+	"github.com/copo888/channel_app/common/errorx"
+	"github.com/copo888/channel_app/common/model"
+	"github.com/copo888/channel_app/common/responsex"
+	"github.com/copo888/channel_app/common/typesX"
+	"github.com/copo888/channel_app/wanlisinpay/internal/payutils"
+	"github.com/copo888/channel_app/wanlisinpay/internal/svc"
+	"github.com/copo888/channel_app/wanlisinpay/internal/types"
+	"github.com/gioco-play/gozzle"
+	"github.com/zeromicro/go-zero/core/logx"
+	"go.opentelemetry.io/otel/trace"
+	"net/url"
+)
+
+type PayOrderLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewPayOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) PayOrderLogic {
+	return PayOrderLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *PayOrderLogic) PayOrder(req *types.PayOrderRequest) (resp *types.PayOrderResponse, err error) {
+
+	logx.Infof("Enter PayOrder. channelName: %s, PayOrderRequest: %v", l.svcCtx.Config.ProjectName, req)
+
+	// 取得取道資訊
+	var channel typesX.ChannelData
+	channelModel := model.NewChannel(l.svcCtx.MyDB)
+	if channel, err = channelModel.GetChannelByProjectName(l.svcCtx.Config.ProjectName); err != nil {
+		return
+	}
+
+	// 檢查 userId
+	if req.PayType == "YK" && len(req.UserId) == 0 {
+		logx.Errorf("userId不可为空 userId:%s", req.UserId)
+		return nil, errorx.New(responsex.INVALID_USER_ID)
+	}
+
+	// 取值
+	notifyUrl := l.svcCtx.Config.Server + "/api/pay-call-back"
+	//notifyUrl = "http://b2d4-211-75-36-190.ngrok.io/api/pay-call-back"
+	//timestamp := time.Now().Format("20060102150405")
+	//ip := utils.GetRandomIp()
+	//randomID := utils.GetRandomString(12, utils.ALL, utils.MIX)
+
+	// 組請求參數
+	data := url.Values{}
+	data.Set("bid", channel.MerId)
+	data.Set("money", req.TransactionAmount)
+	data.Set("order_sn", req.OrderNo)
+	data.Set("notify_url", notifyUrl)
+	data.Set("pay_type", req.PayType)
+
+	// 組請求參數 FOR JSON
+
+	// 加簽
+	sign := payutils.SortAndSignFromUrlValues(data, channel.MerKey)
+	data.Set("sign", sign)
+
+	// 請求渠道
+	logx.Infof("支付下单请求地址:%s,支付請求參數:%#v", channel.PayUrl, data)
+	span := trace.SpanFromContext(l.ctx)
+	//res, ChnErr := gozzle.Post(channel.PayUrl).Timeout(10).Trace(span).JSON(data)
+	res, ChnErr := gozzle.Post(channel.PayUrl).Timeout(10).Trace(span).Form(data)
+	logx.Infof("Status: %d  Body: %s", res.Status(), string(res.Body()))
+	if ChnErr != nil {
+		return nil, errorx.New(responsex.SERVICE_RESPONSE_ERROR, ChnErr.Error())
+	}
+	// 渠道回覆處理 [請依照渠道返回格式 自定義]
+	channelResp := struct {
+		Code int    `json:"code"`
+		Msg  string `json:"msg, optional"`
+		Data struct {
+			Url string `json:"url,optional"`
+		} `json:"data"`
+		Time float64 `json:"time"`
+	}{}
+
+	// 返回body 轉 struct
+	if err = res.DecodeJSON(&channelResp); err != nil {
+		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, err.Error())
+	} else if channelResp.Code != 100 { // 渠道狀態碼判斷
+		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, channelResp.Msg)
+	}
+
+	// 若需回傳JSON 請自行更改
+	//if strings.EqualFold(req.JumpType, "json") {
+	//	amount, err2 := strconv.ParseFloat(channelResp.Money, 64)
+	//	if err2 != nil {
+	//		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, err2.Error())
+	//	}
+	//	// 返回json
+	//	receiverInfoJson, err3 := json.Marshal(types.ReceiverInfoVO{
+	//		CardName:   channelResp.PayInfo.Name,
+	//		CardNumber: channelResp.PayInfo.Card,
+	//		BankName:   channelResp.PayInfo.Bank,
+	//		BankBranch: channelResp.PayInfo.Subbranch,
+	//		Amount:     amount,
+	//		Link:       "",
+	//		Remark:     "",
+	//	})
+	//	if err3 != nil {
+	//		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, err3.Error())
+	//	}
+	//	return &types.PayOrderResponse{
+	//		PayPageType:    "json",
+	//		PayPageInfo:    string(receiverInfoJson),
+	//		ChannelOrderNo: "",
+	//		IsCheckOutMer:  false, // 自組收銀台回傳 true
+	//	}, nil
+	//}
+
+	resp = &types.PayOrderResponse{
+		PayPageType:    "url",
+		PayPageInfo:    channelResp.Data.Url,
+		ChannelOrderNo: "",
+	}
+
+	return
+}
---
+++ wanlisinpay/internal/logic/proxypayquerybalancelogic.go
@@ -0,0 +1,83 @@
+package logic
+
+import (
+	"context"
+	"github.com/copo888/channel_app/common/errorx"
+	model2 "github.com/copo888/channel_app/common/model"
+	"github.com/copo888/channel_app/common/responsex"
+	"github.com/copo888/channel_app/wanlisinpay/internal/payutils"
+	"github.com/gioco-play/gozzle"
+	"go.opentelemetry.io/otel/trace"
+	"net/url"
+	"time"
+
+	"github.com/copo888/channel_app/wanlisinpay/internal/svc"
+	"github.com/copo888/channel_app/wanlisinpay/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ProxyPayQueryBalanceLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewProxyPayQueryBalanceLogic(ctx context.Context, svcCtx *svc.ServiceContext) ProxyPayQueryBalanceLogic {
+	return ProxyPayQueryBalanceLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *ProxyPayQueryBalanceLogic) ProxyPayQueryBalance() (resp *types.ProxyPayQueryInternalBalanceResponse, err error) {
+
+	logx.Infof("Enter ProxyPayQueryBalance. channelName: %s", l.svcCtx.Config.ProjectName)
+
+	channelModel := model2.NewChannel(l.svcCtx.MyDB)
+	channel, err1 := channelModel.GetChannelByProjectName(l.svcCtx.Config.ProjectName)
+	if err1 != nil {
+		return nil, errorx.New(responsex.INVALID_PARAMETER, err1.Error())
+	}
+
+	data := url.Values{}
+	data.Set("partner", channel.MerId)
+	data.Set("service", "10201")
+
+	// 加簽
+	sign := payutils.SortAndSignFromUrlValues(data, channel.MerKey)
+	data.Set("sign", sign)
+
+	// 請求渠道
+	logx.Infof("代付查单请求地址:%s,代付請求參數:%#v", channel.ProxyPayQueryUrl, data)
+	span := trace.SpanFromContext(l.ctx)
+	ChannelResp, ChnErr := gozzle.Post(channel.ProxyPayQueryBalanceUrl).Timeout(10).Trace(span).Form(data)
+	logx.Infof("Status: %d  Body: %s", ChannelResp.Status(), string(ChannelResp.Body()))
+	if ChnErr != nil {
+		logx.Error("渠道返回錯誤: ", ChnErr.Error())
+		return nil, errorx.New(responsex.SERVICE_RESPONSE_ERROR, ChnErr.Error())
+	}
+
+	// 渠道回覆處理 [請依照渠道返回格式 自定義]
+	balanceQueryResp := struct {
+		Success bool   `json:"success"`
+		Msg     string `json:"msg"`
+		Balance string `json:"balance"`
+	}{}
+
+	if err3 := ChannelResp.DecodeJSON(&balanceQueryResp); err3 != nil {
+		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, err3.Error())
+	} else if balanceQueryResp.Success != true {
+		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, balanceQueryResp.Msg)
+	}
+
+	resp = &types.ProxyPayQueryInternalBalanceResponse{
+		ChannelNametring:   channel.Name,
+		ChannelCodingtring: channel.Code,
+		ProxyPayBalance:    balanceQueryResp.Balance,
+		UpdateTimetring:    time.Now().Format("2006-01-02 15:04:05"),
+	}
+
+	return
+}
