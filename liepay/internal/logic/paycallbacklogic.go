bebce0a549627063b4f34966c06cfcfcdc7ef9fb
allen@e-pay.vip <allen@e-pay.vip>
2022-06-30 21:29:56 +0800

    修正LIEPAY 回调

--- liepay/internal/logic/paycallbacklogic.go
+++ liepay/internal/logic/paycallbacklogic.go
@@ -12,7 +12,6 @@ import (
 	"github.com/copo888/channel_app/liepay/internal/payutils"
 	"github.com/gioco-play/gozzle"
 	"go.opentelemetry.io/otel/trace"
-	"strconv"
 	"time"
 
 	"github.com/copo888/channel_app/liepay/internal/svc"
@@ -56,11 +55,6 @@ func (l *PayCallBackLogic) PayCallBack(req *types.PayCallBackRequest) (resp stri
 		return "fail", errorx.New(responsex.INVALID_SIGN)
 	}
 
-	var realAmount float64
-	if realAmount, err = strconv.ParseFloat(req.RealAmount, 64); err != nil {
-		return "fail", errorx.New(responsex.INVALID_AMOUNT)
-	}
-
 	orderStatus := "1"
 	if req.Status == 2 {
 		orderStatus = "20"
@@ -70,7 +64,7 @@ func (l *PayCallBackLogic) PayCallBack(req *types.PayCallBackRequest) (resp stri
 		PayOrderNo:     req.MchOrderNo,
 		ChannelOrderNo: req.TransOrderNo, // 渠道訂單號 (若无则填入->"CHN_" + orderNo)
 		OrderStatus:    orderStatus,        // 若渠道只有成功会回调 固定 20:成功; 訂單狀態(1:处理中 20:成功 )
-		OrderAmount:    realAmount,
+		OrderAmount:    req.RealAmount,
 		CallbackTime:   time.Now().Format("20060102150405"),
 	}
 
