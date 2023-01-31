c5c3573f858089ec8dca1d4b38775fb0b6cc2a83
Eddie <>
2023-01-31 13:21:24 +0800

    init

--- common/constants/redisKey/redisKey.go
+++ common/constants/redisKey/redisKey.go
@@ -5,4 +5,5 @@ const (
 	CACHE_ORDER_DATA_REDIRECT = "cache:orderRedirect:data:" // 返回渠道导向的功能
 	CACHE_PAY_ORDER_CHANNEL_BANK = "cache:payChannelBank:data:" // 自組收銀台暫存訊息
 	CACHE_PAY_ORDER_CHANNEL_REDIRECT = "cache:payChannelRedirect:data:" // 渠道导向参数
+	CACHE_PAY_ORDER_CHANNEL_REDIRECT_VA = "cache:payChannelRedirectVA:data:" // 渠道导向参数
 )
