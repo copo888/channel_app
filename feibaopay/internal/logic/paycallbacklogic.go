6715886659c319c34cc7292a6d2974a9887ad9ef
Eddie <>
2023-03-03 15:08:55 +0800

    fix 修改回调

--- feibaopay/internal/logic/paycallbacklogic.go
+++ feibaopay/internal/logic/paycallbacklogic.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/copo888/channel_app/common/apimodel/bo"
+	"github.com/copo888/channel_app/common/apimodel/vo"
 	"github.com/copo888/channel_app/common/constants"
 	"github.com/copo888/channel_app/common/errorx"
 	model2 "github.com/copo888/channel_app/common/model"
@@ -13,8 +15,11 @@ import (
 	"github.com/copo888/channel_app/feibaopay/internal/payutils"
 	"github.com/copo888/channel_app/feibaopay/internal/svc"
 	"github.com/copo888/channel_app/feibaopay/internal/types"
+	"github.com/gioco-play/gozzle"
 	"github.com/zeromicro/go-zero/core/logx"
 	"go.opentelemetry.io/otel/trace"
+	"strconv"
+	"time"
 )
 
 type PayCallBackLogic struct {
@@ -85,50 +90,51 @@ func (l *PayCallBackLogic) PayCallBack(req *types.PayCallBackRequest) (resp stri
 
 	errj := json.Unmarshal([]byte(desString),&desOrder)
 	if errj != nil {
+		logx.WithContext(l.ctx).Errorf("支付回调解析json失败, error : "+errj.Error())
 		return "fail", errj
 	}
-	//var orderAmount float64
-	//if orderAmount, err = strconv.ParseFloat(desOrder.Amount, 64); err != nil {
-	//	return "fail", errorx.New(responsex.INVALID_AMOUNT)
-	//}
-	//
-	//orderStatus := "1"
-	//if desOrder.Status == "success" {
-	//	orderStatus = "20"
-	//}
-	//
-	//payCallBackBO := bo.PayCallBackBO{
-	//	PayOrderNo:     req.MerchantOrderNum,
-	//	ChannelOrderNo: "CHN_"+desOrder.MerchantOrderNum, // 渠道訂單號 (若无则填入->"CHN_" + orderNo)
-	//	OrderStatus:    orderStatus,        // 若渠道只有成功会回调 固定 20:成功; 訂單狀態(1:处理中 20:成功 )
-	//	OrderAmount:    orderAmount,
-	//	CallbackTime:   time.Now().Format("20060102150405"),
-	//}
-	//
-	///** 回調至 merchant service **/
-	//span := trace.SpanFromContext(l.ctx)
-	//// 組密鑰
-	//payKey, errk := utils.MicroServiceEncrypt(l.svcCtx.Config.ApiKey.PayKey, l.svcCtx.Config.ApiKey.PublicKey)
-	//if errk != nil {
-	//	return "fail", errorx.New(responsex.GENERAL_EXCEPTION, err.Error())
-	//}
-	//
-	//url := fmt.Sprintf("%s:%d/dior/merchant-api/pay-call-back", l.svcCtx.Config.Merchant.Host, l.svcCtx.Config.Merchant.Port)
-	//res, errx := gozzle.Post(url).Timeout(20).Trace(span).Header("authenticationPaykey", payKey).JSON(payCallBackBO)
-	//logx.WithContext(l.ctx).Info("回调后资讯: ", res)
-	//if errx != nil {
-	//	return "err", errorx.New(responsex.GENERAL_EXCEPTION, err.Error())
-	//} else if res.Status() != 200 {
-	//	return "err", errorx.New(responsex.INVALID_STATUS_CODE, fmt.Sprintf("status:%d", res.Status()))
-	//}
-	//
-	//// 處理res
-	//payCallBackVO := vo.BoadminRespVO{}
-	//if err = res.DecodeJSON(&payCallBackVO); err != nil {
-	//	return "err", err
-	//} else if payCallBackVO.Code != "0" {
-	//	return "err", errorx.New(payCallBackVO.Code)
-	//}
+	var orderAmount float64
+	if orderAmount, err = strconv.ParseFloat(desOrder.Amount, 64); err != nil {
+		return "fail", errorx.New(responsex.INVALID_AMOUNT)
+	}
+
+	orderStatus := "1"
+	if desOrder.Status == "success" {
+		orderStatus = "20"
+	}
+
+	payCallBackBO := bo.PayCallBackBO{
+		PayOrderNo:     req.MerchantOrderNum,
+		ChannelOrderNo: "CHN_"+desOrder.MerchantOrderNum, // 渠道訂單號 (若无则填入->"CHN_" + orderNo)
+		OrderStatus:    orderStatus,        // 若渠道只有成功会回调 固定 20:成功; 訂單狀態(1:处理中 20:成功 )
+		OrderAmount:    orderAmount,
+		CallbackTime:   time.Now().Format("20060102150405"),
+	}
+
+	/** 回調至 merchant service **/
+	span := trace.SpanFromContext(l.ctx)
+	// 組密鑰
+	payKey, errk := utils.MicroServiceEncrypt(l.svcCtx.Config.ApiKey.PayKey, l.svcCtx.Config.ApiKey.PublicKey)
+	if errk != nil {
+		return "fail", errorx.New(responsex.GENERAL_EXCEPTION, err.Error())
+	}
+
+	url := fmt.Sprintf("%s:%d/dior/merchant-api/pay-call-back", l.svcCtx.Config.Merchant.Host, l.svcCtx.Config.Merchant.Port)
+	res, errx := gozzle.Post(url).Timeout(20).Trace(span).Header("authenticationPaykey", payKey).JSON(payCallBackBO)
+	logx.WithContext(l.ctx).Info("回调后资讯: ", res)
+	if errx != nil {
+		return "err", errorx.New(responsex.GENERAL_EXCEPTION, err.Error())
+	} else if res.Status() != 200 {
+		return "err", errorx.New(responsex.INVALID_STATUS_CODE, fmt.Sprintf("status:%d", res.Status()))
+	}
+
+	// 處理res
+	payCallBackVO := vo.BoadminRespVO{}
+	if err = res.DecodeJSON(&payCallBackVO); err != nil {
+		return "err", err
+	} else if payCallBackVO.Code != "0" {
+		return "err", errorx.New(payCallBackVO.Code)
+	}
 
 	return "success", nil
 }
--- feibaopay/internal/logic/proxypaycallbacklogic.go
+++ feibaopay/internal/logic/proxypaycallbacklogic.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/copo888/channel_app/common/apimodel/bo"
 	"github.com/copo888/channel_app/common/constants"
 	"github.com/copo888/channel_app/common/errorx"
 	model2 "github.com/copo888/channel_app/common/model"
@@ -11,9 +12,14 @@ import (
 	"github.com/copo888/channel_app/common/typesX"
 	"github.com/copo888/channel_app/common/utils"
 	"github.com/copo888/channel_app/feibaopay/internal/payutils"
+	"github.com/gioco-play/gozzle"
+	"go.opentelemetry.io/otel/trace"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/copo888/channel_app/feibaopay/internal/svc"
 	"github.com/copo888/channel_app/feibaopay/internal/types"
-	"go.opentelemetry.io/otel/trace"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -85,48 +91,49 @@ func (l *ProxyPayCallBackLogic) ProxyPayCallBack(req *types.ProxyPayCallBackRequ
 
 	errj := json.Unmarshal([]byte(desString),&desOrder)
 	if errj != nil {
-		return "fail",errj
+		logx.WithContext(l.ctx).Errorf("代付回调解析json失败, error : "+errj.Error())
+		return "fail", errj
 	}
-	//var orderAmount float64
-	//if orderAmount, err = strconv.ParseFloat(desOrder.Amount, 64); err != nil {
-	//	return "fail", errorx.New(responsex.INVALID_SIGN)
-	//}
-	//var status = "0" //渠道回調狀態(0:處理中1:成功2:失敗)
-	//if desOrder.Status == "success" {
-	//	status = "1"
-	//} else if strings.Index("fail,fail_done,reverted", desOrder.Status) > -1 {
-	//	status = "2"
-	//}
+	var orderAmount float64
+	if orderAmount, err = strconv.ParseFloat(desOrder.Amount, 64); err != nil {
+		return "fail", errorx.New(responsex.INVALID_SIGN)
+	}
+	var status = "0" //渠道回調狀態(0:處理中1:成功2:失敗)
+	if desOrder.Status == "success" {
+		status = "1"
+	} else if strings.Index("fail,fail_done,reverted", desOrder.Status) > -1 {
+		status = "2"
+	}
 
-	//proxyPayCallBackBO := &bo.ProxyPayCallBackBO{
-	//	ProxyPayOrderNo:     req.MerchantOrderNum,
-	//	ChannelOrderNo:      "",
-	//	ChannelResultAt:     time.Now().Format("20060102150405"),
-	//	ChannelResultStatus: status,
-	//	ChannelResultNote:   req.Msg,
-	//	Amount:              orderAmount,
-	//	ChannelCharge:       0,
-	//	UpdatedBy:           "",
-	//}
+	proxyPayCallBackBO := &bo.ProxyPayCallBackBO{
+		ProxyPayOrderNo:     req.MerchantOrderNum,
+		ChannelOrderNo:      "",
+		ChannelResultAt:     time.Now().Format("20060102150405"),
+		ChannelResultStatus: status,
+		ChannelResultNote:   req.Msg,
+		Amount:              orderAmount,
+		ChannelCharge:       0,
+		UpdatedBy:           "",
+	}
 
 	// call boadmin callback api
-	//span := trace.SpanFromContext(l.ctx)
-	//payKey, errk := utils.MicroServiceEncrypt(l.svcCtx.Config.ApiKey.ProxyKey, l.svcCtx.Config.ApiKey.PublicKey)
-	//if errk != nil {
-	//	return "fail", errorx.New(responsex.GENERAL_EXCEPTION, err.Error())
-	//}
-	//
-	////BoProxyRespVO := &vo.BoadminProxyRespVO{}
-	//url := fmt.Sprintf("%s:%d/dior/merchant-api/proxy-call-back", l.svcCtx.Config.Merchant.Host, l.svcCtx.Config.Merchant.Port)
-	//
-	//res, errx := gozzle.Post(url).Timeout(20).Trace(span).Header("authenticationProxykey", payKey).JSON(proxyPayCallBackBO)
-	//logx.WithContext(l.ctx).Info("回调后资讯: ", res)
-	//if errx != nil {
-	//	logx.WithContext(l.ctx).Error(errx.Error())
-	//	return "fail", errorx.New(responsex.GENERAL_EXCEPTION, err.Error())
-	//} else if res.Status() != 200 {
-	//	return "fail", errorx.New(responsex.INVALID_STATUS_CODE, fmt.Sprintf("status:%d", res.Status()))
-	//}
+	span := trace.SpanFromContext(l.ctx)
+	payKey, errk := utils.MicroServiceEncrypt(l.svcCtx.Config.ApiKey.ProxyKey, l.svcCtx.Config.ApiKey.PublicKey)
+	if errk != nil {
+		return "fail", errorx.New(responsex.GENERAL_EXCEPTION, err.Error())
+	}
+
+	//BoProxyRespVO := &vo.BoadminProxyRespVO{}
+	url := fmt.Sprintf("%s:%d/dior/merchant-api/proxy-call-back", l.svcCtx.Config.Merchant.Host, l.svcCtx.Config.Merchant.Port)
+
+	res, errx := gozzle.Post(url).Timeout(20).Trace(span).Header("authenticationProxykey", payKey).JSON(proxyPayCallBackBO)
+	logx.WithContext(l.ctx).Info("回调后资讯: ", res)
+	if errx != nil {
+		logx.WithContext(l.ctx).Error(errx.Error())
+		return "fail", errorx.New(responsex.GENERAL_EXCEPTION, err.Error())
+	} else if res.Status() != 200 {
+		return "fail", errorx.New(responsex.INVALID_STATUS_CODE, fmt.Sprintf("status:%d", res.Status()))
+	}
 	//else if errDecode:= res.DecodeJSON(BoProxyRespVO); errDecode!=nil {
 	//  return "fail",errorx.New(responsex.DECODE_JSON_ERROR)
 	//} else if BoProxyRespVO.Code != "000"{
