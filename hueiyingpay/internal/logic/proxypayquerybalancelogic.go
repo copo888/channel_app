cd55d012b5c2f68be4946cdd20f727ae598a1ea6
allen@e-pay.vip <allen@e-pay.vip>
2023-03-08 16:46:16 +0800

    修正余额金额错误

--- hueiyingpay/internal/logic/proxypayquerybalancelogic.go
+++ hueiyingpay/internal/logic/proxypayquerybalancelogic.go
@@ -6,6 +6,7 @@ import (
 	"github.com/copo888/channel_app/common/errorx"
 	model2 "github.com/copo888/channel_app/common/model"
 	"github.com/copo888/channel_app/common/responsex"
+	"github.com/copo888/channel_app/common/utils"
 	"github.com/copo888/channel_app/hueiyingpay/internal/payutils"
 	"github.com/gioco-play/gozzle"
 	"go.opentelemetry.io/otel/trace"
@@ -79,10 +80,12 @@ func (l *ProxyPayQueryBalanceLogic) ProxyPayQueryBalance() (resp *types.ProxyPay
 		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR)
 	}
 
+
+
 	resp = &types.ProxyPayQueryInternalBalanceResponse{
 		ChannelNametring:   channel.Name,
 		ChannelCodingtring: channel.Code,
-		ProxyPayBalance:    fmt.Sprintf("%f", balanceQueryResp.AvailableAgentpayBalance),
+		ProxyPayBalance:    fmt.Sprintf("%f", utils.FloatDivF(balanceQueryResp.AvailableAgentpayBalance, 100)),
 		UpdateTimetring:    time.Now().Format("2006-01-02 15:04:05"),
 	}
 
