4bd920d4eae1901228fc812ce871bad2f1cdd733
Eddie <>
2023-03-09 16:02:17 +0800

    feat 修改肥宝回调

--- feibaopay/internal/logic/proxypaycallbacklogic.go
+++ feibaopay/internal/logic/proxypaycallbacklogic.go
@@ -44,7 +44,7 @@ func (l *ProxyPayCallBackLogic) ProxyPayCallBack(req *types.ProxyPayCallBackRequ
 
 	logx.WithContext(l.ctx).Infof("Enter ProxyPayCallBack. channelName: %s, orderNo: %s, ProxyPayCallBackRequest: %+v", l.svcCtx.Config.ProjectName, req.MerchantOrderNum, req)
 
-	iv := "c11fa9ed92344d9d"
+	iv := "edbeaacdb228dcac"
 
 	//寫入交易日志
 	if err := utils.CreateTransactionLog(l.svcCtx.MyDB, &typesX.TransactionLogData{
