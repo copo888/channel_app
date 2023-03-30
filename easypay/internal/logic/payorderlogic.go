4b9db3ae014e0819947d33235791aae1efb05c55
Eddie <>
2023-03-30 16:05:25 +0800

    fix 修改支付取得bankCode逻辑

--- easypay/internal/logic/payorderlogic.go
+++ easypay/internal/logic/payorderlogic.go
@@ -48,14 +48,18 @@ func (l *PayOrderLogic) PayOrder(req *types.PayOrderRequest) (resp *types.PayOrd
 	if channel, err = channelModel.GetChannelByProjectName(l.svcCtx.Config.ProjectName); err != nil {
 		return
 	}
-	channelBankMap, err2 := model.NewChannelBank(l.svcCtx.MyDB).GetChannelBankCode(l.svcCtx.MyDB, channel.Code, req.BankCode)
-	if err2 != nil { //BankName空: COPO沒有對應銀行(要加bk_banks)，MapCode為空: 渠道沒有對應銀行代碼(要加ch_channel_banks)
-		logx.WithContext(l.ctx).Errorf("銀行代碼抓取資料錯誤:%s", err2.Error())
-		return nil, errorx.New(responsex.BANK_CODE_INVALID, "银行代码: "+req.BankCode, "银行名称: "+channelBankMap.BankName, "渠道Map名称: "+channelBankMap.MapCode)
-	} else if channelBankMap.BankName == "" || channelBankMap.MapCode == "" {
-		logx.WithContext(l.ctx).Errorf("银行代码: %s,银行名称: %s,渠道银行代码: %s", req.BankCode, channelBankMap.BankName, channelBankMap.MapCode)
-		return nil, errorx.New(responsex.BANK_CODE_INVALID, "银行代码: "+req.BankCode, "银行名称: "+channelBankMap.BankName, "渠道Map名称: "+channelBankMap.MapCode)
+	var channelBankMap *typesX.BankCodeMapX
+	if req.BankCode != "" {
+		channelBankMap, err = model.NewChannelBank(l.svcCtx.MyDB).GetChannelBankCode(l.svcCtx.MyDB, channel.Code, req.BankCode)
+		if err != nil { //BankName空: COPO沒有對應銀行(要加bk_banks)，MapCode為空: 渠道沒有對應銀行代碼(要加ch_channel_banks)
+			logx.WithContext(l.ctx).Errorf("銀行代碼抓取資料錯誤:%s", err.Error())
+			return nil, errorx.New(responsex.BANK_CODE_INVALID, "银行代码: "+req.BankCode, "银行名称: "+channelBankMap.BankName, "渠道Map名称: "+channelBankMap.MapCode)
+		} else if channelBankMap.BankName == "" || channelBankMap.MapCode == "" {
+			logx.WithContext(l.ctx).Errorf("银行代码: %s,银行名称: %s,渠道银行代码: %s", req.BankCode, channelBankMap.BankName, channelBankMap.MapCode)
+			return nil, errorx.New(responsex.BANK_CODE_INVALID, "银行代码: "+req.BankCode, "银行名称: "+channelBankMap.BankName, "渠道Map名称: "+channelBankMap.MapCode)
+		}
 	}
+
 
 	/** UserId 必填時使用 **/
 	if strings.EqualFold(req.PayType, "YK") && len(req.UserId) == 0 {
