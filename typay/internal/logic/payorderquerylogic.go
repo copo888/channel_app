91003db2622beacf38983426230532ca0027bc7d
Eddie <>
2023-05-03 16:50:57 +0800

    init

---
+++ typay/internal/logic/payorderquerylogic.go
@@ -0,0 +1,136 @@
+package logic
+
+import (
+	"context"
+	"fmt"
+	"github.com/copo888/channel_app/common/errorx"
+	"github.com/copo888/channel_app/common/model"
+	"github.com/copo888/channel_app/common/responsex"
+	"github.com/copo888/channel_app/common/typesX"
+	"github.com/copo888/channel_app/common/utils"
+	"github.com/copo888/channel_app/typay/internal/payutils"
+	"github.com/copo888/channel_app/typay/internal/svc"
+	"github.com/copo888/channel_app/typay/internal/types"
+	"github.com/gioco-play/gozzle"
+	"go.opentelemetry.io/otel/trace"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PayOrderQueryLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	traceID string
+}
+
+func NewPayOrderQueryLogic(ctx context.Context, svcCtx *svc.ServiceContext) PayOrderQueryLogic {
+	return PayOrderQueryLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		traceID: trace.SpanContextFromContext(ctx).TraceID().String(),
+	}
+}
+
+func (l *PayOrderQueryLogic) PayOrderQuery(req *types.PayOrderQueryRequest) (resp *types.PayOrderQueryResponse, err error) {
+
+	logx.WithContext(l.ctx).Infof("Enter PayOrderQuery. channelName: %s, PayOrderQueryRequest: %+v", l.svcCtx.Config.ProjectName, req)
+
+	// 取得取道資訊
+	var channel typesX.ChannelData
+	channelModel := model.NewChannel(l.svcCtx.MyDB)
+	if channel, err = channelModel.GetChannelByProjectName(l.svcCtx.Config.ProjectName); err != nil {
+		return
+	}
+	randomID := utils.GetRandomString(12, utils.ALL, utils.MIX)
+	timestamp := time.Now().Unix()
+	// 組請求參數
+	data := url.Values{}
+	data.Set("time", strconv.FormatInt(timestamp, 10))
+	data.Set("nonce", randomID)
+	data.Set("tid", req.OrderNo)
+	data.Set("appid", channel.MerId)
+
+
+	// 組請求參數 FOR JSON
+	//data := struct {
+	//	merchId  string
+	//	orderId  string
+	//	time     string
+	//	signType string
+	//	sign     string
+	//}{
+	//	merchId:  channel.MerId,
+	//	orderId:  req.OrderNo,
+	//	time:     timestamp,
+	//	signType: "MD5",
+	//}
+
+	// 加簽
+	sign := payutils.SortAndSignFromUrlValues(data, channel.MerKey, l.ctx)
+	data.Set("sign", sign)
+
+	// 加簽 JSON
+	//sign := payutils.SortAndSignFromObj(data, channel.MerKey)
+	//data.sign = sign
+
+	// 請求渠道
+	logx.WithContext(l.ctx).Infof("支付查詢请求地址:%s,支付請求參數:%v", channel.PayQueryUrl, data)
+
+	span := trace.SpanFromContext(l.ctx)
+	res, chnErr := gozzle.Post(channel.PayQueryUrl).Header("server", "true").Timeout(20).Trace(span).Form(data)
+	//res, ChnErr := gozzle.Post(channel.PayQueryUrl).Timeout(20).Trace(span).JSON(data)
+
+	if chnErr != nil {
+		return nil, errorx.New(responsex.SERVICE_RESPONSE_DATA_ERROR, err.Error())
+	} else if res.Status() != 200 {
+		logx.WithContext(l.ctx).Infof("Status: %d  Body: %s", res.Status(), string(res.Body()))
+		return nil, errorx.New(responsex.INVALID_STATUS_CODE, fmt.Sprintf("Error HTTP Status: %d", res.Status()))
+	}
+	logx.WithContext(l.ctx).Infof("Status: %d  Body: %s", res.Status(), string(res.Body()))
+
+	// 渠道回覆處理
+	channelResp := struct {
+		Code  int `json:"code"`
+		Msg   string `json:"msg"`
+		Data  struct{
+			Appid string `json:"appid"`
+			Merchanttradeno string `json:"merchanttradeno"`
+			Tradeno string `json:"tradeno"`
+			Money string `json:"money"`
+			Applytime string `json:"applytime"`
+			Completetime string `json:"completetime"`
+			Tradestatus string `json:"tradestatus"`
+			Sign string `json:"sign"`
+		} `json:"data"`
+	}{}
+
+	if err = res.DecodeJSON(&channelResp); err != nil {
+		return nil, errorx.New(responsex.GENERAL_EXCEPTION, err.Error())
+	} else if channelResp.Code != 1 {
+		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, channelResp.Msg)
+	}
+
+	orderAmount, errParse := strconv.ParseFloat(channelResp.Data.Money, 64)
+	if errParse != nil {
+		return nil, errorx.New(responsex.GENERAL_EXCEPTION, errParse.Error())
+	}
+
+	orderStatus := "0"
+	if channelResp.Data.Tradestatus == "PAYMENT_SUCCESS" { //PAYMENT_FAILURE 支付失败  PAYMENT_SUCCESS 支付成功
+		orderStatus = "1"
+	}else if channelResp.Data.Tradestatus == "PAYMENT_FAILURE" {
+		orderStatus = "2"
+	}
+
+	resp = &types.PayOrderQueryResponse{
+		OrderAmount: orderAmount,
+		OrderStatus: orderStatus, //订单状态: 状态 0处理中，1成功，2失败
+	}
+
+	return
+}
