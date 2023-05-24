4e995e441425b0e6e904c3d0e0c84ba8966e0b57
Kenny <kenny@e-pay.vip>
2023-05-24 16:51:21 +0800

    fix proxy

--- testpay/internal/logic/proxypayorderlogic.go
+++ testpay/internal/logic/proxypayorderlogic.go
@@ -2,8 +2,7 @@ package logic
 
 import (
 	"context"
-	"github.com/copo888/channel_app/common/errorx"
-	"github.com/copo888/channel_app/common/responsex"
+	"github.com/copo888/channel_app/common/utils"
 	"github.com/copo888/channel_app/testpay/internal/svc"
 	"github.com/copo888/channel_app/testpay/internal/types"
 
@@ -26,11 +25,10 @@ func NewProxyPayOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) Prox
 
 func (l *ProxyPayOrderLogic) ProxyPayOrder(req *types.ProxyPayOrderRequest) (*types.ProxyPayOrderResponse, error) {
 	logx.WithContext(l.ctx).Infof("Enter ProxyPayOrder. channelName: %s, ProxyPayOrderRequest: %#v", l.svcCtx.Config.ProjectName, req)
-	//resp := &types.ProxyPayOrderResponse{
-	//	ChannelOrderNo: utils.GetRandomString(10, 0, 0),
-	//	OrderStatus:    "20",
-	//}
+	resp := &types.ProxyPayOrderResponse{
+		ChannelOrderNo: utils.GetRandomString(10, 0, 0),
+		OrderStatus:    "20",
+	}
 
-	return nil, errorx.New(responsex.TIMEOUT_CHANNEL, "Post \"https://globalgopay.com/api/payment\": context deadline exceeded (Client.Timeout exceeded while awaiting headers)")
-	//return resp, nil
+	return resp, nil
 }
