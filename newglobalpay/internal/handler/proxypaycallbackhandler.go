91ec40078b2642a6a9a4b4d9a0e728ef373d70bd
allen@e-pay.vip <allen@e-pay.vip>
2023-05-28 17:35:13 +0800

    fix

--- newglobalpay/internal/handler/proxypaycallbackhandler.go
+++ newglobalpay/internal/handler/proxypaycallbackhandler.go
@@ -23,7 +23,7 @@ func ProxyPayCallBackHandler(ctx *svc.ServiceContext) http.HandlerFunc {
 
 		var req types.ProxyPayCallBackRequest
 
-		if err := httpx.ParseJsonBody(r, &req); err != nil {
+		if err := httpx.ParseForm(r, &req); err != nil {
 			responsex.Json(w, r, responsex.FAIL, nil, err)
 			return
 		}
