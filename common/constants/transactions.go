6b0cdddb9c136845584ab371d3e79eebf8fa7c04
Henry <henry@wooplanets.com>
2023-09-14 16:30:26 +0800

    feat: 增加交易日誌類型

--- common/constants/transactions.go
+++ common/constants/transactions.go
@@ -2,13 +2,15 @@ package constants
 
 const (
 	//交易日志类型
-	ERROR_MSG                 = "1" //1:錯誤訊息
-	MERCHANT_REQUEST          = "2" //2:商户请求
-	ERROR_REPLIED_TO_MERCHANT = "3" //3:返回商户错误
-	DATA_REQUEST_CHANNEL      = "4" //4.打给渠道资料
-	RESPONSE_FROM_CHANNEL     = "5" //5.渠道返回资料
-	CALLBACK_FROM_CHANNEL     = "6" //6.渠道回调资料
-	CALLBACK_TO_MERCHANT      = "7" //7.回调给商户
+	ERROR_MSG                  = "1" //1:錯誤訊息
+	MERCHANT_REQUEST           = "2" //2:商户请求
+	ERROR_REPLIED_TO_MERCHANT  = "3" //3:返回商户错误
+	DATA_REQUEST_CHANNEL       = "4" //4.打给渠道资料
+	RESPONSE_FROM_CHANNEL      = "5" //5.渠道返回资料
+	CALLBACK_FROM_CHANNEL      = "6" //6.渠道回调资料
+	CALLBACK_TO_MERCHANT       = "7" //7.回调给商户
+	RESPONSE_FROM_MERCHANT     = "8" //8.商户返回资料
+	ERROR_REPLIED_FROM_CHANNEL = "9" //9:渠道返回错误
 
 	//日誌來源(1:內充平台、2:支付API、3:代付API、4:代付平台、5:下發API)
 	PLATEFORM_NC = "1"
