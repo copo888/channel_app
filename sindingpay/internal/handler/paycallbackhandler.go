f9f32c13335894e387cc92ab13b54a5f64d07a0d
Ken <kenny@e-pay.vip>
2024-06-11 11:57:25 +0800

    new sindingpay X1

--- sindingpay/internal/handler/paycallbackhandler.go
+++ sindingpay/internal/handler/paycallbackhandler.go
@@ -9,9 +9,9 @@ import (
 	"github.com/copo888/channel_app/sindingpay/internal/types"
 	"github.com/thinkeridea/go-extend/exnet"
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/rest/httpx"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
-	"io"
 	"net/http"
 )
 
@@ -23,32 +23,16 @@ func PayCallBackHandler(ctx *svc.ServiceContext) http.HandlerFunc {
 
 		var req types.PayCallBackRequest
 
-		bodyBytes, err := io.ReadAll(r.Body)
-
-		if err != nil {
-			responsex.Json(w, r, responsex.FAIL, nil, err)
-			return
-		}
-
-		logx.WithContext(r.Context()).Infof("PayOrder enter: %s", string(bodyBytes))
-
-		if err := json.Unmarshal(bodyBytes, &req); err != nil {
+		if err := httpx.ParseJsonBody(r, &req); err != nil {
 			responsex.Json(w, r, responsex.DECODE_JSON_ERROR, nil, err)
 			return
 		}
-
-		//if err := httpx.ParseJsonBody(r, &req); err != nil {
-		//	responsex.Json(w, r, responsex.DECODE_JSON_ERROR, nil, err)
-		//	return
-		//}
-
 		// Form 格式
 		//if err := httpx.ParseForm(r, &req); err != nil {
 		//	responsex.Json(w, r, responsex.FAIL, nil, err)
 		//	return
 		//}
-
-		logx.WithContext(r.Context()).Infof("支付回调 : %+v", req)
+		logx.WithContext(r.Context()).Infof("支付回调: %+v", req)
 
 		if err := vaildx.Validator.Struct(req); err != nil {
 			responsex.Json(w, r, responsex.INVALID_PARAMETER, nil, err)
--- sindingpay/internal/handler/proxypaycallbackhandler.go
+++ sindingpay/internal/handler/proxypaycallbackhandler.go
@@ -9,9 +9,9 @@ import (
 	"github.com/copo888/channel_app/sindingpay/internal/types"
 	"github.com/thinkeridea/go-extend/exnet"
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/rest/httpx"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
-	"io"
 	"net/http"
 )
 
@@ -23,21 +23,11 @@ func ProxyPayCallBackHandler(ctx *svc.ServiceContext) http.HandlerFunc {
 
 		var req types.ProxyPayCallBackRequest
 
-		bodyBytes, err := io.ReadAll(r.Body)
-
-		if err != nil {
+		if err := httpx.ParseJsonBody(r, &req); err != nil {
 			responsex.Json(w, r, responsex.FAIL, nil, err)
 			return
 		}
-
-		logx.WithContext(r.Context()).Infof("ProxyPayOrder enter: %s", string(bodyBytes))
-
-		if err := json.Unmarshal(bodyBytes, &req); err != nil {
-			responsex.Json(w, r, responsex.DECODE_JSON_ERROR, nil, err)
-			return
-		}
-
-		logx.WithContext(r.Context()).Infof("%+v", req)
+		logx.WithContext(r.Context()).Infof("代付回调: +v", req)
 
 		if err := vaildx.Validator.Struct(req); err != nil {
 			responsex.Json(w, r, responsex.INVALID_PARAMETER, nil, err)
