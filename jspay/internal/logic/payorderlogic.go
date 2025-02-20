f27860aea82cfa54869e7ae24132e728f99bc3f0
kennypro <kenny@wooplanets.com>
2025-02-20 17:53:37 +0800

    new jspay1

--- jspay/internal/logic/payorderlogic.go
+++ jspay/internal/logic/payorderlogic.go
@@ -49,16 +49,22 @@ func (l *PayOrderLogic) PayOrder(req *types.PayOrderRequest) (resp *types.PayOrd
 	notifyUrl := l.svcCtx.Config.Server + "/api/pay-call-back"
 	//notifyUrl = "http://b2d4-211-75-36-190.ngrok.io/api/pay-call-back"
 
+	if len(req.UserId) == 0 {
+		logx.WithContext(l.ctx).Errorf("userId不可为空 userId:%s", req.UserId)
+		return nil, errorx.New(responsex.INVALID_USER_ID)
+	}
+
 	// 組請求參數 FOR JSON
 	data := struct {
 		Amount      string `json:"amount"`
 		CallbackUrl string `json:"callback_url"`
 		OutTradeNo  string `json:"out_trade_no"`
-		//PaidName    string `json:"paid_name"`
+		PaidName    string `json:"paid_name"`
 	}{
 		Amount:      req.TransactionAmount,
 		CallbackUrl: notifyUrl,
 		OutTradeNo:  req.OrderNo,
+		PaidName:    req.UserId,
 	}
 
 	//寫入交易日志
