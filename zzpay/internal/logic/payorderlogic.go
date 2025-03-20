d8227d737fa4085faa5a5e8133804c197c2ec959
kennypro <kenny@wooplanets.com>
2025-03-20 14:26:29 +0800

    new zzpay

---
+++ zzpay/internal/logic/payorderlogic.go
@@ -0,0 +1,111 @@
+package logic
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/copo888/channel_app/common/constants"
+	"github.com/copo888/channel_app/common/errorx"
+	"github.com/copo888/channel_app/common/model"
+	"github.com/copo888/channel_app/common/responsex"
+	"github.com/copo888/channel_app/common/typesX"
+	"github.com/copo888/channel_app/common/utils"
+	"github.com/copo888/channel_app/zzpay/internal/service"
+	"github.com/copo888/channel_app/zzpay/internal/svc"
+	"github.com/copo888/channel_app/zzpay/internal/types"
+	"github.com/zeromicro/go-zero/core/logx"
+	"go.opentelemetry.io/otel/trace"
+	"strconv"
+	"strings"
+)
+
+type PayOrderLogic struct {
+	logx.Logger
+	ctx     context.Context
+	svcCtx  *svc.ServiceContext
+	traceID string
+}
+
+func NewPayOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) PayOrderLogic {
+	return PayOrderLogic{
+		Logger:  logx.WithContext(ctx),
+		ctx:     ctx,
+		svcCtx:  svcCtx,
+		traceID: trace.SpanContextFromContext(ctx).TraceID().String(),
+	}
+}
+
+func (l *PayOrderLogic) PayOrder(req *types.PayOrderRequest) (resp *types.PayOrderResponse, err error) {
+
+	logx.WithContext(l.ctx).Infof("Enter PayOrder. channelName: %s, PayOrderRequest: %#v", l.svcCtx.Config.ProjectName, req)
+	service.CallTGSendURL(l.ctx, l.svcCtx, &types.TelegramNotifyRequest{ChatID: l.svcCtx.Config.TelegramSend.ChatId, Message: "測試"})
+	// 取得取道資訊
+	channelModel := model.NewChannel(l.svcCtx.MyDB)
+	channel, err1 := channelModel.GetChannelByProjectName(l.svcCtx.Config.ProjectName)
+	if err1 != nil {
+		return nil, errorx.New(responsex.INVALID_PARAMETER, err1.Error())
+	}
+
+	//// 渠道狀態碼判斷
+	//if req.BankCode != "1" {
+	//	//寫入交易日志
+	//	if err := utils.CreateTransactionLog(l.svcCtx.MyDB, &typesX.TransactionLogData{
+	//		MerchantNo: req.MerchantId,
+	//		//MerchantOrderNo: req.OrderNo,
+	//		OrderNo:          req.OrderNo,
+	//		LogType:          constants.ERROR_REPLIED_FROM_CHANNEL,
+	//		LogSource:        constants.API_ZF,
+	//		Content:          "Failed",
+	//		TraceId:          l.traceID,
+	//		ChannelErrorCode: "400",
+	//	}); err != nil {
+	//		logx.WithContext(l.ctx).Errorf("写入交易日志错误:%s", err)
+	//	}
+	//	return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, "http: 400, Failed")
+	//}
+
+	//寫入交易日志
+	if err := utils.CreateTransactionLog(l.svcCtx.MyDB, &typesX.TransactionLogData{
+		MerchantNo:      req.MerchantId,
+		MerchantOrderNo: req.MerchantOrderNo,
+		OrderNo:         req.OrderNo,
+		ChannelCode:     channel.Code,
+		LogType:         constants.DATA_REQUEST_CHANNEL,
+		LogSource:       constants.API_ZF,
+		Content:         "Success",
+		TraceId:         l.traceID,
+	}); err != nil {
+		logx.WithContext(l.ctx).Errorf("写入交易日志错误:%s", err)
+	}
+
+	if strings.EqualFold(req.JumpType, "json") {
+		transactionAmount, _ := strconv.ParseFloat(req.TransactionAmount, 64)
+
+		// 返回json
+		receiverInfoJson, err3 := json.Marshal(types.ReceiverInfoVO{
+			CardName:   "王小銘",
+			CardNumber: "11111111111111",
+			BankName:   "工商银行AX",
+			BankBranch: "工商银行XX",
+			Amount:     transactionAmount,
+			Link:       "",
+			Remark:     "",
+		})
+		if err3 != nil {
+			return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, err3.Error())
+		}
+		return &types.PayOrderResponse{
+			PayPageType:    "json",
+			PayPageInfo:    string(receiverInfoJson),
+			ChannelOrderNo: "",
+			IsCheckOutMer:  true, // 自組收銀台回傳 true
+		}, nil
+	}
+
+	resp = &types.PayOrderResponse{
+		PayPageType:    "url",
+		PayPageInfo:    "https://docs.goldenf.vip/",
+		ChannelOrderNo: "",
+	}
+
+	return
+}
---
+++ zzpay/internal/service/telegramNotifyService.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/copo888/channel_app/zzpay/internal/config"
+	"github.com/copo888/channel_app/zzpay/internal/svc"
+	"github.com/copo888/channel_app/zzpay/internal/types"
+	"github.com/gioco-play/gozzle"
+	"github.com/go-redis/redis/v8"
+	"github.com/zeromicro/go-zero/core/logx"
+	"go.opentelemetry.io/otel/trace"
+	"gorm.io/gorm"
+)
+
+type ServiceContext struct {
+	Config      config.Config
+	RedisClient *redis.Client
+	MyDB        *gorm.DB
+}
+
+func CallTGSendURL(ctx context.Context, svcCtx *svc.ServiceContext, message *types.TelegramNotifyRequest) {
+	go func() {
+		DoCallTGSendURL(ctx, svcCtx, message)
+	}()
+}
+
+func DoCallTGSendURL(ctx context.Context, svcCtx *svc.ServiceContext, message *types.TelegramNotifyRequest) error {
+	span := trace.SpanFromContext(ctx)
+	notifyUrl := fmt.Sprintf("%s:%d/telegram/notify", svcCtx.Config.TelegramSend.Host, svcCtx.Config.TelegramSend.Port)
+	//notifyUrl := fmt.Sprintf("%s:%d/line/send", svcCtx.Config.LineSend.Host, svcCtx.Config.LineSend.Port)
+
+	if _, err := gozzle.Post(notifyUrl).Timeout(25).Trace(span).JSON(message); err != nil {
+		logx.WithContext(ctx).Errorf("报警通知失敗:%s", err.Error())
+	}
+	return nil
+}
