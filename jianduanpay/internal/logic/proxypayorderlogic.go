3730bad571f717969ead98d70b2854961b5dbb77
kennypro <kenny@wooplanets.com>
2025-04-28 16:29:41 +0800

    new jianduanpay

---
+++ jianduanpay/internal/logic/proxypayorderlogic.go
@@ -0,0 +1,197 @@
+package logic
+
+import (
+	"context"
+	"fmt"
+	"github.com/copo888/channel_app/common/constants"
+	"github.com/copo888/channel_app/common/errorx"
+	model2 "github.com/copo888/channel_app/common/model"
+	"github.com/copo888/channel_app/common/responsex"
+	"github.com/copo888/channel_app/common/typesX"
+	"github.com/copo888/channel_app/common/utils"
+	"github.com/copo888/channel_app/jianduanpay/internal/payutils"
+	"github.com/copo888/channel_app/jianduanpay/internal/service"
+	"github.com/gioco-play/gozzle"
+	"go.opentelemetry.io/otel/trace"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/copo888/channel_app/jianduanpay/internal/svc"
+	"github.com/copo888/channel_app/jianduanpay/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ProxyPayOrderLogic struct {
+	logx.Logger
+	ctx     context.Context
+	svcCtx  *svc.ServiceContext
+	traceID string
+}
+
+func NewProxyPayOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) ProxyPayOrderLogic {
+	return ProxyPayOrderLogic{
+		Logger:  logx.WithContext(ctx),
+		ctx:     ctx,
+		svcCtx:  svcCtx,
+		traceID: trace.SpanContextFromContext(ctx).TraceID().String(),
+	}
+}
+
+func (l *ProxyPayOrderLogic) ProxyPayOrder(req *types.ProxyPayOrderRequest) (*types.ProxyPayOrderResponse, error) {
+
+	logx.WithContext(l.ctx).Infof("Enter ProxyPayOrder. channelName: %s,orderNo: %s, ProxyPayOrderRequest: %+v", l.svcCtx.Config.ProjectName, req.OrderNo, req)
+
+	// 取得取道資訊
+	channelModel := model2.NewChannel(l.svcCtx.MyDB)
+	channel, err1 := channelModel.GetChannelByProjectName(l.svcCtx.Config.ProjectName)
+
+	if err1 != nil {
+		return nil, errorx.New(responsex.INVALID_PARAMETER, err1.Error())
+	}
+	//channelBankMap, err2 := model2.NewChannelBank(l.svcCtx.MyDB).GetChannelBankCode(l.svcCtx.MyDB, channel.Code, req.ReceiptCardBankCode)
+	//if err2 != nil { //BankName空: COPO沒有對應銀行(要加bk_banks)，MapCode為空: 渠道沒有對應銀行代碼(要加ch_channel_banks)
+	//	logx.WithContext(l.ctx).Errorf("銀行代碼抓取資料錯誤:%s", err2.Error())
+	//	return nil, errorx.New(responsex.BANK_CODE_INVALID, "银行代码: "+req.ReceiptCardBankCode, "银行名称: "+req.ReceiptCardBankName, "渠道Map名称: "+channelBankMap.MapCode)
+	//} else if channelBankMap.BankName == "" || channelBankMap.MapCode == "" {
+	//	logx.WithContext(l.ctx).Errorf("银行代码: %s,银行名称: %s,渠道银行代码: %s", req.ReceiptCardBankCode, req.ReceiptCardBankName, channelBankMap.MapCode)
+	//	return nil, errorx.New(responsex.BANK_CODE_INVALID, "银行代码: "+req.ReceiptCardBankCode, "银行名称: "+req.ReceiptCardBankName, "渠道Map名称: "+channelBankMap.MapCode)
+	//}
+	// 組請求參數
+	//amountFloat, _ := strconv.ParseFloat(req.TransactionAmount, 64)
+	//transactionAmount := strconv.FormatFloat(amountFloat, 'f', 2, 64)
+
+	data := url.Values{}
+	data.Set("merNo", channel.MerId)
+	data.Set("ordernum", req.OrderNo)
+	data.Set("bankname", req.ReceiptCardBankName)
+	data.Set("bankCode", "101") //channelBankMap.MapCode)
+	data.Set("bankcard", req.ReceiptAccountNumber)
+	data.Set("amount", req.TransactionAmount)
+	data.Set("name", req.ReceiptAccountName)
+	data.Set("notifyurl", l.svcCtx.Config.Server+"/api/proxy-pay-call-back")
+
+	// 加簽
+	sign := payutils.GetSign(channel.MerId + req.OrderNo + req.ReceiptCardBankName + req.TransactionAmount + channel.MerKey)
+	data.Set("sign", sign)
+
+	//寫入交易日志
+	if err := utils.CreateTransactionLog(l.svcCtx.MyDB, &typesX.TransactionLogData{
+		MerchantNo:      req.MerchantId,
+		MerchantOrderNo: req.MerchantOrderNo,
+		ChannelCode:     channel.Code,
+		OrderNo:         req.OrderNo,
+		LogType:         constants.DATA_REQUEST_CHANNEL,
+		LogSource:       constants.API_DF,
+		Content:         fmt.Sprintf("%+v", data),
+		TraceId:         l.traceID,
+	}); err != nil {
+		logx.WithContext(l.ctx).Errorf("写入交易日志错误:%s", err)
+	}
+
+	// 請求渠道
+	logx.WithContext(l.ctx).Infof("代付下单请求地址:%s,請求參數:%+v", channel.ProxyPayUrl, data)
+	span := trace.SpanFromContext(l.ctx)
+	ChannelResp, ChnErr := gozzle.Post(channel.ProxyPayUrl).Timeout(20).Trace(span).Form(data)
+
+	if ChnErr != nil {
+		logx.WithContext(l.ctx).Error("渠道返回錯誤: ", ChnErr.Error())
+		msg := fmt.Sprintf("代付提单，呼叫'%s'渠道返回錯誤: '%s'，订单号： '%s'", channel.Name, ChnErr.Error(), req.OrderNo)
+		service.CallTGSendURL(l.ctx, l.svcCtx, &types.TelegramNotifyRequest{ChatID: l.svcCtx.Config.TelegramSend.ChatId, Message: msg})
+
+		//寫入交易日志
+		if err := utils.CreateTransactionLog(l.svcCtx.MyDB, &typesX.TransactionLogData{
+			MerchantNo:       req.MerchantId,
+			ChannelCode:      channel.Code,
+			MerchantOrderNo:  req.MerchantOrderNo,
+			OrderNo:          req.OrderNo,
+			LogType:          constants.ERROR_REPLIED_FROM_CHANNEL,
+			LogSource:        constants.API_DF,
+			Content:          ChnErr.Error(),
+			TraceId:          l.traceID,
+			ChannelErrorCode: ChnErr.Error(),
+		}); err != nil {
+			logx.WithContext(l.ctx).Errorf("写入交易日志错误:%s", err)
+		}
+
+		return nil, errorx.New(responsex.SERVICE_RESPONSE_ERROR, ChnErr.Error())
+	} else if ChannelResp.Status() != 200 {
+		logx.WithContext(l.ctx).Infof("Status: %d  Body: %s", ChannelResp.Status(), string(ChannelResp.Body()))
+		msg := fmt.Sprintf("代付提单，呼叫'%s'渠道返回Http状态码錯誤: '%d'，订单号： '%s'", channel.Name, ChannelResp.Status(), req.OrderNo)
+		service.CallTGSendURL(l.ctx, l.svcCtx, &types.TelegramNotifyRequest{ChatID: l.svcCtx.Config.TelegramSend.ChatId, Message: msg})
+
+		//寫入交易日志
+		if err := utils.CreateTransactionLog(l.svcCtx.MyDB, &typesX.TransactionLogData{
+			MerchantNo:       req.MerchantId,
+			MerchantOrderNo:  req.MerchantOrderNo,
+			ChannelCode:      channel.Code,
+			OrderNo:          req.OrderNo,
+			LogType:          constants.ERROR_REPLIED_FROM_CHANNEL,
+			LogSource:        constants.API_DF,
+			Content:          string(ChannelResp.Body()),
+			TraceId:          l.traceID,
+			ChannelErrorCode: strconv.Itoa(ChannelResp.Status()),
+		}); err != nil {
+			logx.WithContext(l.ctx).Errorf("写入交易日志错误:%s", err)
+		}
+
+		return nil, errorx.New(responsex.INVALID_STATUS_CODE, fmt.Sprintf("Error HTTP Status: %d", ChannelResp.Status()))
+	}
+	logx.WithContext(l.ctx).Infof("Status: %d  Body: %s", ChannelResp.Status(), string(ChannelResp.Body()))
+	// 渠道回覆處理 [請依照渠道返回格式 自定義]
+	channelResp := struct {
+		Status string `json:"status"`
+		Msg    string `json:"msg"`
+	}{}
+
+	if err := ChannelResp.DecodeJSON(&channelResp); err != nil {
+		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, err.Error())
+	}
+
+	if strings.Index(channelResp.Msg, "大于可用余额") > -1 {
+		logx.WithContext(l.ctx).Errorf("代付渠提单道返回错误: %s: %s", channelResp.Status, channelResp.Msg)
+		return nil, errorx.New(responsex.INSUFFICIENT_IN_AMOUNT, channelResp.Msg)
+	} else if channelResp.Status != "success" {
+		logx.WithContext(l.ctx).Errorf("代付渠道返回错误: %s: %s", channelResp.Status, channelResp.Msg)
+
+		//寫入交易日志
+		if err := utils.CreateTransactionLog(l.svcCtx.MyDB, &typesX.TransactionLogData{
+			MerchantNo:       req.MerchantId,
+			MerchantOrderNo:  req.MerchantOrderNo,
+			ChannelCode:      channel.Code,
+			OrderNo:          req.OrderNo,
+			LogType:          constants.ERROR_REPLIED_FROM_CHANNEL,
+			LogSource:        constants.API_DF,
+			Content:          fmt.Sprintf("%+v", channelResp),
+			TraceId:          l.traceID,
+			ChannelErrorCode: channelResp.Status,
+		}); err != nil {
+			logx.WithContext(l.ctx).Errorf("写入交易日志错误:%s", err)
+		}
+
+		return nil, errorx.New(responsex.CHANNEL_REPLY_ERROR, channelResp.Msg)
+	}
+
+	//寫入交易日志
+	if err := utils.CreateTransactionLog(l.svcCtx.MyDB, &typesX.TransactionLogData{
+		MerchantNo:      req.MerchantId,
+		MerchantOrderNo: req.MerchantOrderNo,
+		ChannelCode:     channel.Code,
+		OrderNo:         req.OrderNo,
+		LogType:         constants.RESPONSE_FROM_CHANNEL,
+		LogSource:       constants.API_DF,
+		Content:         fmt.Sprintf("%+v", channelResp),
+		TraceId:         l.traceID,
+	}); err != nil {
+		logx.WithContext(l.ctx).Errorf("写入交易日志错误:%s", err)
+	}
+
+	//組返回給backOffice 的代付返回物件
+	resp := &types.ProxyPayOrderResponse{
+		ChannelOrderNo: "CHN_" + req.OrderNo,
+		OrderStatus:    "",
+	}
+
+	return resp, nil
+}
