9cc353d82505ef807a3e9803fdcb98c32c5c6319
ken <kenny@wooplanets.com>
2025-07-17 11:10:12 +0800

    new testpayu

---
+++ testpayu/internal/logic/proxypayorderlogic.go
@@ -0,0 +1,34 @@
+package logic
+
+import (
+	"context"
+	"github.com/copo888/channel_app/common/utils"
+	"github.com/copo888/channel_app/testpayu/internal/svc"
+	"github.com/copo888/channel_app/testpayu/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ProxyPayOrderLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewProxyPayOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) ProxyPayOrderLogic {
+	return ProxyPayOrderLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *ProxyPayOrderLogic) ProxyPayOrder(req *types.ProxyPayOrderRequest) (*types.ProxyPayOrderResponse, error) {
+	logx.WithContext(l.ctx).Infof("Enter ProxyPayOrder. channelName: %s, ProxyPayOrderRequest: %#v", l.svcCtx.Config.ProjectName, req)
+	resp := &types.ProxyPayOrderResponse{
+		ChannelOrderNo: utils.GetRandomString(10, 0, 0),
+		OrderStatus:    "20",
+	}
+	//TEST4
+	return resp, nil
+}
